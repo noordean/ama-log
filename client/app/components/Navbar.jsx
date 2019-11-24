a4ce74c26f5b8f2ce56ee276edad21e41c8d92a1
noordean <ebroyeem90@gmail.com>
2019-11-24 10:53:15 +0100

    Add correct Navbar.jsx

---
+++ client/app/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { productCategories, onProductClick, selectedId } = this.props;
+
+    return (
+      <div className="navbar">
+        <div className="ui grid">
+          <div className="fourteen wide column">
+            <div className="ui text menu">
+              <a className="browse item">
+                Browse Products
+                <i className="dropdown icon"></i>
+              </a>
+            </div>
+            <div className="ui flowing basic admission popup">
+              <div className="ui three column relaxed divided grid">
+                {productCategories.map(prodCat => (
+                  <div className="column" key={prodCat.category.id}>
+                    <h4 className="ui header">{prodCat.category.name}</h4>
+                    <div className="ui link list">
+                      {prodCat.products.map(prod => (
+                        <a
+                          className="item"
+                          key={prod.id}
+                          onClick={() => onProductClick(prod.id)}
+                          style={{
+                            color: prod.id === selectedId ? "#4183c4" : ""
+                          }}
+                        >
+                          {prod.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="two wide column right aligned">
+            <a
+              href="https://docs.google.com/forms/d/1yn0sA5FLCcy-IrYq9OkqOYMGtPH0UCKV0P3ZU5L2R2U"
+              target="_blank"
+            >
+              Fill our service form
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
